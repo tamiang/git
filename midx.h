0a9ac138c7cdd7fe971e3563d6459c9265c718f5
Derrick Stolee <dstolee@microsoft.com>
2018-06-21 10:45:21 -0400

    multi-pack-index: load into memory
    Create a new multi_pack_index struct for loading multi-pack-indexes into
    memory. Create a test-tool builtin for reading basic information about
    that multi-pack-index to verify the correct data is written.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- midx.h
+++ midx.h
@@ -1,6 +1,10 @@
 #ifndef __MIDX_H__
 #define __MIDX_H__
 
+struct multi_pack_index;
+
+struct multi_pack_index *load_multi_pack_index(const char *object_dir);
+
 int write_midx_file(const char *object_dir);
 
 #endif
--- t/t5319-multi-pack-index.sh
+++ t/t5319-multi-pack-index.sh
@@ -3,9 +3,19 @@
 test_description='multi-pack-indexes'
 . ./test-lib.sh
 
+midx_read_expect () {
+	cat >expect <<-EOF
+	header: 4d494458 1 0 0
+	object_dir: .
+	EOF
+	test-tool read-midx . >actual &&
+	test_cmp expect actual
+}
+
 test_expect_success 'write midx with no packs' '
 	git multi-pack-index --object-dir=. write &&
-	test_path_is_file pack/multi-pack-index
+	test_path_is_file pack/multi-pack-index &&
+	midx_read_expect
 '
 
 test_done
