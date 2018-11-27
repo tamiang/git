4abd829a45a24ed4ca770c799f95081823efecf0
Derrick Stolee <dstolee@microsoft.com>
2018-11-27 13:51:38 -0500

    tree-walk-sparse: a sparser way to walk objects
    This speeds up 'git push' from a client that changed very little.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- tree-walk-sparse.c
+++ tree-walk-sparse.c
@@ -25,11 +25,11 @@ struct names_and_oids {
 	struct string_list list;
 };
 
-void insert_name_and_oid(struct names_and_oids *no,
+static void insert_name_and_oid(struct names_and_oids *no,
 			 const char *name,
-			 struct tree *tree)
+			 const struct object_id *oid)
 {
-	struct string_list_item *item = string_list_insert(&no->list, name)->util;
+	struct string_list_item *item = string_list_insert(&no->list, name);
 	struct oidset *set; 
 	
 	if (!item->util) {
@@ -40,14 +40,14 @@ void insert_name_and_oid(struct names_and_oids *no,
 		set = item->util;
 	}
 
-	oidset_insert(set, &tree->object.oid);
+	oidset_insert(set, oid);
 }
 
-void init_names_and_oids(struct names_and_oids *no) {
+static  void init_names_and_oids(struct names_and_oids *no) {
 	string_list_init(&no->list, 1);
 }
 
-void free_names_and_oids(struct names_and_oids *no) {
+static void free_names_and_oids(struct names_and_oids *no) {
 	int i; 
 	for (i = 0; i < no->list.nr; i++) {
 		oidset_clear(no->list.items[i].util);
@@ -77,8 +77,9 @@ static void mark_tree_uninteresting_shallow(struct tree *tree)
 	/* don't recurse now! */
 }
 
-static void mark_tree_contents_uninteresting(struct repository *r,
-					     struct tree *tree)
+static void walk_tree_contents(struct repository *r,
+			       struct tree *tree,
+			       struct names_and_oids *no)
 {
 	struct tree_desc desc;
 	struct name_entry entry;
@@ -90,14 +91,17 @@ static void mark_tree_contents_uninteresting(struct repository *r,
 	while (tree_entry(&desc, &entry)) {
 		switch (object_type(entry.mode)) {
 		case OBJ_TREE:
-			/* add to dictionary here! */
-			mark_tree_uninteresting_shallow(lookup_tree(r, entry.oid));
+			insert_name_and_oid(no, entry.path, entry.oid);
+
+			if (tree->object.flags & UNINTERESTING)
+				mark_tree_uninteresting_shallow(lookup_tree(r, entry.oid));
 			break;
 		case OBJ_BLOB:
-			mark_blob_uninteresting(lookup_blob(r, entry.oid));
+			if (tree->object.flags & UNINTERESTING)
+				mark_blob_uninteresting(lookup_blob(r, entry.oid));
 			break;
 		default:
-			/* Subprojct commit - not in this repository */
+			/* Subproject commit - not in this repository */
 			break;
 		}
 	}
@@ -105,18 +109,60 @@ static void mark_tree_contents_uninteresting(struct repository *r,
 	free_tree_buffer(tree);
 }
 
+static void tree_walk_sparse(struct rev_info *revs,
+	       		     struct oidset *set)
+{
+	int i, has_interesting = 0, has_uninteresting = 0;
+	struct names_and_oids no;
+	struct object_id *oid;
+	struct oidset_iter iter;
+	init_names_and_oids(&no);
+
+	/* Check if we need to recurse down these trees */
+	oidset_iter_init(set, &iter);
+	while ((oid = oidset_iter_next(&iter))) {
+		struct tree *tree = lookup_tree(revs->repo, oid);
+		if (tree->object.flags & UNINTERESTING)
+			has_uninteresting = 1;
+		else
+			has_interesting = 1;
+	}
+
+	if (!has_interesting || !has_uninteresting) {
+		return;
+	}
+
+	/* Phase 1: read all trees in the set, add trees to dictionary */
+	oidset_iter_init(set, &iter);
+	while ((oid = oidset_iter_next(&iter))) {
+		struct tree *tree = lookup_tree(revs->repo, oid);
+		walk_tree_contents(revs->repo, tree, &no);
+	}
+
+	/* Phase 2: for each path, recurse on that oidset */
+	for (i = 0; i < no.list.nr; i++)
+		tree_walk_sparse(revs, (struct oidset *)no.list.items[i].util);
+
+	free_names_and_oids(&no);
+}
+
 static void mark_edge_parents_uninteresting(struct commit *commit,
 					    struct rev_info *revs,
-					    show_edge_fn show_edge)
+					    show_edge_fn show_edge,
+					    struct oidset *set)
 {
 	struct commit_list *parents;
 
 	for (parents = commit->parents; parents; parents = parents->next) {
 		struct commit *parent = parents->item;
+		struct tree *tree;
 
 		if (!(parent->object.flags & UNINTERESTING))
 			continue;
-		mark_tree_uninteresting_shallow(get_commit_tree(parent));
+
+		tree = get_commit_tree(parent);
+		oidset_insert(set, &tree->object.oid);
+		mark_tree_uninteresting_shallow(tree);
 
 		if (!(parent->object.flags & SHOWN)) {
 			parent->object.flags |= SHOWN;
@@ -130,14 +176,15 @@ void mark_edges_uninteresting_sparse(struct rev_info *revs,
 {
 	struct commit_list *list;
 	int i;
+	struct oidset set;
+	oidset_init(&set, 16);
 
 	for (list = revs->commits; list; list = list->next) {
 		struct commit *commit = list->item;
+		oidset_insert(&set, get_commit_tree_oid(commit));
 
 		if (commit->object.flags & UNINTERESTING) {
 			mark_tree_uninteresting_shallow(get_commit_tree(commit));
-
-			/* TODO: add tree to list */
 
 			if (revs->edge_hint_aggressive && !(commit->object.flags & SHOWN)) {
 				commit->object.flags |= SHOWN;
@@ -147,10 +194,11 @@ void mark_edges_uninteresting_sparse(struct rev_info *revs,
 		}
 
 		/* send tree list here */
-		mark_edge_parents_uninteresting(commit, revs, show_edge);
+		mark_edge_parents_uninteresting(commit, revs, show_edge, &set);
 	}
 
-	/* TODO: mark the trees uninteresting using the sparse algorithm */
+	tree_walk_sparse(revs, &set);
+	oidset_clear(&set);
 
 	if (revs->edge_hint_aggressive) {
 		for (i = 0; i < revs->cmdline.nr; i++) {
