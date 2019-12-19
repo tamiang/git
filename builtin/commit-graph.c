bff5f221cbfadc581770940486e01d65cf6d54d4
Garima Singh <garima.singh@microsoft.com>
2019-12-19 15:59:22 -0500

    commit-graph: add --changed-paths option to write
    Add --changed-paths option to git commit-graph write. This option will
    soon allow users to compute bloom filters for the paths changed between
    a commit and its first significant parent, and write this information
    into the commit-graph file.
    
    Note: This commit does not change any behavior. It only introduces
    the option and passes down the appropriate flag to the commit-graph.
    
    RFC Notes:
    1. We named the option --changed-paths to capture what the option does,
       instead of how it does it. The current implementation does this
       using bloom filters. We believe using --changed-paths however keeps
       the implementation open to other data structures.
       All thoughts and suggestions for the name and this approach are
       welcome
    
    2. Currently, a subsequent commit in this series will add tests that
       exercise this option. I plan to split that test commit across the
       series as appropriate.
    
    Helped-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Garima Singh <garima.singh@microsoft.com>

--- builtin/commit-graph.c
+++ builtin/commit-graph.c
@@ -11,7 +11,7 @@ static char const * const builtin_commit_graph_usage[] = {
 	N_("git commit-graph [--object-dir <objdir>]"),
 	N_("git commit-graph read [--object-dir <objdir>]"),
 	N_("git commit-graph verify [--object-dir <objdir>] [--shallow] [--[no-]progress]"),
-	N_("git commit-graph write [--object-dir <objdir>] [--append|--split] [--reachable|--stdin-packs|--stdin-commits] [--[no-]progress] <split options>"),
+	N_("git commit-graph write [--object-dir <objdir>] [--append|--split] [--reachable|--stdin-packs|--stdin-commits] [--changed-paths] [--[no-]progress] <split options>"),
 	NULL
 };
 
@@ -26,7 +26,7 @@ static const char * const builtin_commit_graph_read_usage[] = {
 };
 
 static const char * const builtin_commit_graph_write_usage[] = {
-	N_("git commit-graph write [--object-dir <objdir>] [--append|--split] [--reachable|--stdin-packs|--stdin-commits] [--[no-]progress] <split options>"),
+	N_("git commit-graph write [--object-dir <objdir>] [--append|--split] [--reachable|--stdin-packs|--stdin-commits] [--changed-paths] [--[no-]progress] <split options>"),
 	NULL
 };
 
@@ -39,6 +39,7 @@ static struct opts_commit_graph {
 	int split;
 	int shallow;
 	int progress;
+	int enable_bloom_filters;
 } opts;
 
 static int graph_verify(int argc, const char **argv)
@@ -177,6 +178,8 @@ static int graph_write(int argc, const char **argv)
 			N_("start walk at commits listed by stdin")),
 		OPT_BOOL(0, "append", &opts.append,
 			N_("include all commits already in the commit-graph file")),
+		OPT_BOOL(0, "changed-paths", &opts.enable_bloom_filters,
+			N_("enable computation for changed paths")),
 		OPT_BOOL(0, "progress", &opts.progress, N_("force progress reporting")),
 		OPT_BOOL(0, "split", &opts.split,
 			N_("allow writing an incremental commit-graph file")),
@@ -210,6 +213,8 @@ static int graph_write(int argc, const char **argv)
 		flags |= COMMIT_GRAPH_WRITE_SPLIT;
 	if (opts.progress)
 		flags |= COMMIT_GRAPH_WRITE_PROGRESS;
+	if (opts.enable_bloom_filters)
+		flags |= COMMIT_GRAPH_WRITE_BLOOM_FILTERS;
 
 	read_replace_refs = 0;
 
