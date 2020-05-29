47b26ea61aad76421b5e61a1a6b42095b4bd7caa
SZEDER Gábor <szeder.dev@gmail.com>
2020-05-29 10:50:35 +0200

    t7007-show: make the first test compatible with the next patch
    Otherwise it would fail with GIT_TEST_COMMIT_GRAPH=1.

--- t/t7007-show.sh
+++ t/t7007-show.sh
@@ -4,16 +4,15 @@ test_description='git show'
 
 . ./test-lib.sh
 
-test_expect_success setup '
+test_expect_success 'showing a tag that points at a missing object' '
+	test_when_finished "git tag -d foo-tag" &&
 	echo hello world >foo &&
 	H=$(git hash-object -w foo) &&
 	git tag -a foo-tag -m "Tags $H" $H &&
 	HH=$(expr "$H" : "\(..\)") &&
 	H38=$(expr "$H" : "..\(.*\)") &&
-	rm -f .git/objects/$HH/$H38
-'
+	rm -f .git/objects/$HH/$H38 &&
 
-test_expect_success 'showing a tag that point at a missing object' '
 	test_must_fail git --no-pager show foo-tag
 '
 
