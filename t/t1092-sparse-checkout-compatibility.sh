0b8ab7d38555c5b1051bf8d0b5555ebd228fe0bc
Derrick Stolee <dstolee@microsoft.com>
2021-01-15 10:10:05 -0500

    t1092: compare sparse-checkout to sparse-index
    Add a new 'sparse-index' repo alongside the 'full-checkout' and
    'sparse-checkout' repos in t1092-sparse-checkout-compatibility.sh. Also
    add run_on_sparse and test_sparse_match helpers.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- t/t1092-sparse-checkout-compatibility.sh
+++ t/t1092-sparse-checkout-compatibility.sh
@@ -87,16 +87,25 @@ init_repos () {
 
 	cp -r initial-repo sparse-checkout &&
 	git -C sparse-checkout reset --hard &&
-	git -C sparse-checkout sparse-checkout init --cone &&
+
+	cp -r initial-repo sparse-index &&
+	git -C sparse-index reset --hard &&
 
 	# initialize sparse-checkout definitions
-	git -C sparse-checkout sparse-checkout set deep
+	git -C sparse-checkout sparse-checkout init --cone &&
+	git -C sparse-checkout sparse-checkout set deep &&
+	GIT_TEST_SPARSE_INDEX=1 git -C sparse-index sparse-checkout init --cone &&
+	GIT_TEST_SPARSE_INDEX=1 git -C sparse-index sparse-checkout set deep
 }
 
 run_on_sparse () {
 	(
 		cd sparse-checkout &&
 		$* >../sparse-checkout-out 2>../sparse-checkout-err
+	) &&
+	(
+		cd sparse-index &&
+		$* >../sparse-index-out 2>../sparse-index-err
 	)
 }
 
@@ -113,6 +122,17 @@ test_all_match () {
 	test_cmp full-checkout-out sparse-checkout-out &&
 	test_cmp full-checkout-err sparse-checkout-err
 }
+
+test_sparse_match () {
+	run_on_sparse $* &&
+	test_cmp sparse-checkout-out sparse-index-out &&
+	test_cmp sparse-checkout-err sparse-index-err
+}
+
+test_expect_success 'expanded in-memory index matches full index' '
+	init_repos &&
+	test_sparse_match test-tool read-cache --expand --table-no-stat
+'
 
 test_expect_success 'status with options' '
 	init_repos &&
