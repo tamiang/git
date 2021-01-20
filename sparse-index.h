c3b46c4927452b7878acaf4bd2c67e0345f50fed
Derrick Stolee <dstolee@microsoft.com>
2021-01-20 09:11:01 -0500

    sparse-index: static analysis fixups
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- sparse-index.h
+++ sparse-index.h
@@ -1,6 +1,9 @@
 #ifndef SPARSE_INDEX_H__
 #define SPARSE_INDEX_H__
 
+struct index_state;
+struct repository;
+
 int set_sparse_index_config(struct repository *repo, int enable);
 int convert_to_sparse(struct index_state *istate);
 
