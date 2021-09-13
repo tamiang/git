0aad5d5ccd41d4f3870a5e9cf1058af9558aa9e5
Derrick Stolee <dstolee@microsoft.com>
2021-09-13 13:51:40 -0400

    p2000: add new commands
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- t/perf/p2000-sparse-operations.sh
+++ t/perf/p2000-sparse-operations.sh
@@ -117,5 +117,7 @@ test_perf_on_all git diff
 test_perf_on_all git diff --cached
 test_perf_on_all git blame $SPARSE_CONE/a
 test_perf_on_all git blame $SPARSE_CONE/f3/a
+test_perf_on_all git update-index --add --remove
+test_perf_on_all git sparse-checkout reapply
 
 test_done
