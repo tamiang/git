32b327af9bad08ea6864cd6bf5ff1c26e43c71bd
Derrick Stolee <dstolee@microsoft.com>
2021-10-26 16:01:29 -0400

    snapshots: generate self snapshot as blob
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- refs/snapshots.c
+++ refs/snapshots.c
@@ -8,16 +8,19 @@
 #include "../dir-iterator.h"
 #include "../lockfile.h"
 #include "../object.h"
+#include "../object-store.h"
+#include "../blob.h"
 #include "../repository.h"
 #include "../remote.h"
 #include "../strbuf.h"
 #include "snapshots.h"
+#include "../run-command.h"
 
 static void get_snapshot_dir(const struct repository *repo,
 			     struct strbuf *buf)
 {
 	strbuf_reset(buf);
-	strbuf_addstr(buf, repo->gitdir);
+	strbuf_addstr(buf, repo->commondir);
 	strbuf_addstr(buf, "/snapshots/self/");
 }
 
@@ -35,16 +38,34 @@ int create_ref_snapshot(const struct repository *repo)
 	int result = 0;
 	struct strbuf dirname = STRBUF_INIT;
 	struct strbuf data = STRBUF_INIT;
+	struct child_process cp = CHILD_PROCESS_INIT;
+
 	time_t t = time(NULL);
 
 	get_snapshot_dir(repo, &dirname);
+
+	fprintf(stderr, "dirname: %s\n", dirname.buf);
+
 	safe_create_leading_directories(dirname.buf);
+	mkdir(dirname.buf, 0777);
 
 	strbuf_addf(&data, "%"PRIuMAX"\n", t);
 
 	for_each_ref_in("refs/heads/", append_ref_to_snapshot, &data);
 
-	fprintf(stdout, "%s\n", data.buf);
+	child_process_init(&cp);
+	strvec_pushl(&cp.args, "-C", repo->commondir, "hash-object", "-w", "--stdin", NULL);
+	strvec_pushf(&cp.env_array, DB_ENVIRONMENT "=%s", dirname.buf);
+	cp.in = -1;
+	cp.git_cmd = 1;
+
+	if (start_command(&cp))
+		return -1;
+
+	write_in_full(cp.in, data.buf, data.len);
+	close(cp.in);
+
+	finish_command(&cp);
 
 	strbuf_release(&data);
 	strbuf_release(&dirname);
