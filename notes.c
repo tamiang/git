c4f07816ad5c12840fa4d5a18be7abcddb171870
Derrick Stolee <derrickstolee@github.com>
2023-05-25 15:41:09 -0400

    config: prepare before using notes_ref_name
    The prepare_default_config() method is a wrapper that checks if we have
    loaded default config, and loads it if we have not. Protect uses of
    'notes_ref_name' with these calls to make sure the variable has been
    initialized before use.
    
    Signed-off-by: Derrick Stolee <derrickstolee@github.com>

--- notes.c
+++ notes.c
@@ -992,8 +992,10 @@ const char *default_notes_ref(void)
 	const char *notes_ref = NULL;
 	if (!notes_ref)
 		notes_ref = getenv(GIT_NOTES_REF_ENVIRONMENT);
-	if (!notes_ref)
+	if (!notes_ref) {
+		prepare_default_config();
 		notes_ref = notes_ref_name; /* value of core.notesRef config */
+	}
 	if (!notes_ref)
 		notes_ref = GIT_NOTES_DEFAULT_REF;
 	return notes_ref;
