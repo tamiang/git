0e33f37886bbfcfe9e96c155f5ec7a6e75ce21ed
Jeff Hostetler <jeffhostetler@github.com>
2024-04-29 10:38:17 -0400

    survey: collect the set of requested refs
    Collect the set of requested branches, tags, and etc into a
    ref_array and collect the set of requested patterns into a
    strvec.
    
    Signed-off-by: Jeff Hostetler <jeffhostetler@github.com>

--- builtin/survey.c
+++ builtin/survey.c
@@ -1,11 +1,18 @@
 #include "builtin.h"
 #include "config.h"
 #include "parse-options.h"
+#include "progress.h"
+#include "ref-filter.h"
+#include "strvec.h"
+#include "trace2.h"
 
 static const char * const survey_usage[] = {
 	N_("(EXPERIMENTAL!) git survey <options>"),
 	NULL,
 };
+
+static struct progress *survey_progress = NULL;
+static uint64_t survey_progress_total = 0;
 
 struct survey_refs_wanted {
 	int want_all_refs; /* special override */
@@ -16,6 +23,8 @@ struct survey_refs_wanted {
 	int want_detached;
 	int want_other; /* see FILTER_REFS_OTHERS -- refs/notes/, refs/stash/ */
 };
+
+static struct strvec survey_vec_refs_wanted = STRVEC_INIT;
 
 /*
  * The set of refs that we will search if the user doesn't select
@@ -141,6 +150,83 @@ static void survey_load_config(void)
 	git_config(survey_load_config_cb, NULL);
 }
 
+static void do_load_refs(struct ref_array *ref_array)
+{
+	struct ref_filter filter = REF_FILTER_INIT;
+	struct ref_sorting *sorting;
+	struct string_list sorting_options = STRING_LIST_INIT_DUP;
+
+	string_list_append(&sorting_options, "objectname");
+	sorting = ref_sorting_options(&sorting_options);
+
+	if (survey_opts.refs.want_detached)
+		strvec_push(&survey_vec_refs_wanted, "HEAD");
+
+	if (survey_opts.refs.want_all_refs) {
+		strvec_push(&survey_vec_refs_wanted, "refs/");
+	} else {
+		if (survey_opts.refs.want_branches)
+			strvec_push(&survey_vec_refs_wanted, "refs/heads/");
+		if (survey_opts.refs.want_tags)
+			strvec_push(&survey_vec_refs_wanted, "refs/tags/");
+		if (survey_opts.refs.want_remotes)
+			strvec_push(&survey_vec_refs_wanted, "refs/remotes/");
+		if (survey_opts.refs.want_other) {
+			strvec_push(&survey_vec_refs_wanted, "refs/notes/");
+			strvec_push(&survey_vec_refs_wanted, "refs/stash/");
+		}
+	}
+
+	filter.name_patterns = survey_vec_refs_wanted.v;
+	filter.ignore_case = 0;
+	filter.match_as_path = 1;
+
+	if (survey_opts.show_progress) {
+		survey_progress_total = 0;
+		survey_progress = start_progress(_("Scanning refs..."), 0);
+	}
+
+	filter_refs(ref_array, &filter, FILTER_REFS_KIND_MASK);
+
+	if (survey_opts.show_progress) {
+		survey_progress_total = ref_array->nr;
+		display_progress(survey_progress, survey_progress_total);
+	}
+
+	ref_array_sort(sorting, ref_array);
+
+	if (survey_opts.show_progress)
+		stop_progress(&survey_progress);
+
+	ref_filter_clear(&filter);
+	ref_sorting_release(sorting);
+}
+
+/*
+ * The REFS phase:
+ *
+ * Load the set of requested refs and assess them for scalablity problems.
+ * Use that set to start a treewalk to all reachable objects and assess
+ * them.
+ *
+ * This data will give us insights into the repository itself (the number
+ * of refs, the size and shape of the DAG, the number and size of the
+ * objects).
+ *
+ * Theoretically, this data is independent of the on-disk representation
+ * (e.g. independent of packing concerns).
+ */
+static void survey_phase_refs(struct repository *r)
+{
+	struct ref_array ref_array = { 0 };
+
+	trace2_region_enter("survey", "phase/refs", the_repository);
+	do_load_refs(&ref_array);
+	trace2_region_leave("survey", "phase/refs", the_repository);
+
+	ref_array_clear(&ref_array);
+}
+
 int cmd_survey(int argc, const char **argv, const char *prefix)
 {
 	survey_load_config();
@@ -152,6 +238,10 @@ int cmd_survey(int argc, const char **argv, const char *prefix)
 	if (survey_opts.show_progress < 0)
 		survey_opts.show_progress = isatty(2);
 	fixup_refs_wanted();
+
+	survey_phase_refs(the_repository);
+
+	strvec_clear(&survey_vec_refs_wanted);
 
 	return 0;
 }
