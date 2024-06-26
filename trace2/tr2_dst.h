a6007ea55e1dfca12a99a7ce75e4cd54a41f7dfc
Jeff Hostetler <jeffhostetler@github.com>
2024-06-26 12:27:41 -0400

    trace2: prefetch value of GIT_TRACE2_DST_DEBUG at startup
    Prefetch the value of GIT_TRACE2_DST_DEBUG during startup and before
    we try to open any Trace2 destination pathnames.
    
    Normally, Trace2 always silently fails if a destination target
    cannot be opened so that it doesn't affect the execution of a
    Git command.  The command should run normally, but just not
    generate any trace data.  This can make it difficult to debug
    a telemetry setup, since the user doesn't know why telemetry
    isn't being generated.  If the environment variable
    GIT_TRACE2_DST_DEBUG is true, the Trace2 startup will print
    a warning message with the `errno` to make debugging easier.
    
    However, on Windows, looking up the env variable resets `errno`
    so the warning message always ends with `...tracing: No error`
    which is not very helpful.
    
    Prefetch the env variable at startup.  This avoids the need
    to update each call-site to capture `errno` in the usual
    `saved-errno` variable.
    
    Signed-off-by: Jeff Hostetler <jeffhostetler@github.com>

--- trace2/tr2_dst.h
+++ trace2/tr2_dst.h
@@ -35,4 +35,16 @@ int tr2_dst_trace_want(struct tr2_dst *dst);
  */
 void tr2_dst_write_line(struct tr2_dst *dst, struct strbuf *buf_line);
 
+/*
+ * Return true if we want warning messages when trying to open a
+ * destination.
+ *
+ * (Trace2 always silently fails if a target cannot be opened so that
+ * we don't affect the execution of the Git command, but it is helpful
+ * for debugging telemetry configuration if we log warning messages
+ * when trying to open a target. This is controlled by another config
+ * value.)
+ */
+int tr2_dst_want_warning(void);
+
 #endif /* TR2_DST_H */
