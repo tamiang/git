eaff89d0da7bdfc0d94c75d57d73433ce47834cd
Derrick Stolee <stolee@gmail.com>
2024-09-07 19:43:45 -0400

    pack-objects: add --full-name-hash option
    TODO: ... optimizes to group the same name over time. Better avoids
    collisions.
    
    TODO: create test-tool that will output the histogram of collisions at
    HEAD.
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- t/perf/p5313-pack-objects.sh
+++ t/perf/p5313-pack-objects.sh
@@ -9,33 +9,68 @@ export GIT_TEST_PASSING_SANITIZE_LEAK
 test_perf_large_repo
 
 test_expect_success 'create rev input' '
-	first=1 &&
-	>in &&
-	for parent in $(git rev-list -1 --parents HEAD)
-	do
-		if test $first -ne 1; then
-			printf "^" >>in
-		fi &&
-		first=0 &&
-		echo $parent >>in || return 1
-	done
+	cat >in-thin <<-EOF &&
+	$(git rev-parse HEAD)
+	^$(git rev-parse HEAD~1)
+	EOF
+
+	cat >in-big-recent <<-EOF
+	$(git rev-parse HEAD)
+	^$(git rev-parse HEAD~1000)
+	EOF
 '
 
 test_perf 'thin pack' '
-	git pack-objects --thin --stdout --revs --sparse  <in >out
+	git pack-objects --thin --stdout --revs --sparse  <in-thin >out
+'
+
+test_perf 'thin pack with --full-name-hash' '
+	git pack-objects --thin --stdout --revs --sparse--full-name-hash <in-thin >out
 '
 
 test_perf 'thin pack with --path-walk' '
-	git pack-objects --thin --stdout --revs --sparse --path-walk <in >out
+	git pack-objects --thin --stdout --revs --sparse --path-walk <in-thin >out
 '
 
 test_size 'thin pack' '
-	git pack-objects --thin --stdout --revs --sparse  <in >out &&
+	git pack-objects --thin --stdout --revs --sparse  <in-thin >out &&
+	wc -c <out
+'
+
+test_size 'thin pack with --full-name-hash' '
+	git pack-objects --thin --stdout --revs --sparse --full-name-hash <in-thin >out &&
 	wc -c <out
 '
 
 test_size 'thin pack with --path-walk' '
-	git pack-objects --thin --stdout --revs --sparse --path-walk <in >out &&
+	git pack-objects --thin --stdout --revs --sparse --path-walk <in-thin >out &&
+	wc -c <out
+'
+
+test_perf 'full pack' '
+	git pack-objects --stdout --revs <in-big-recent >out
+'
+
+test_perf 'big recent pack with --full-name-hash' '
+	git pack-objects --stdout --revs --full-name-hash <in-big-recent >out
+'
+
+test_perf 'big recent pack with --path-walk' '
+	git pack-objects --stdout --revs --path-walk <in-big-recent >out
+'
+
+test_size 'big recent pack' '
+	git pack-objects --stdout --revs  <in-big recent >out &&
+	wc -c <out
+'
+
+test_size 'big recent pack with --full-name-hash' '
+	git pack-objects --stdout --revs --full-name-hash <in-big-recent >out &&
+	wc -c <out
+'
+
+test_size 'big recent pack with --path-walk' '
+	git pack-objects --stdout --revs --path-walk <in-big-recent >out &&
 	wc -c <out
 '
 
