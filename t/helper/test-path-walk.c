591438e6dcb59d88b48239d79beee511641e59e7
Derrick Stolee <stolee@gmail.com>
2024-09-18 10:03:03 -0400

    t6601: add helper for testing path-walk API
    Add some tests based on the current behavior, doing interesting checks
    for different sets of branches, ranges, and the --boundary option. This
    sets a baseline for the behavior and we can extend it as new options are
    introduced.
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

---
+++ t/helper/test-path-walk.c
@@ -0,0 +1,77 @@
+#define USE_THE_REPOSITORY_VARIABLE
+
+#include "test-tool.h"
+#include "environment.h"
+#include "hex.h"
+#include "object-name.h"
+#include "object.h"
+#include "pretty.h"
+#include "revision.h"
+#include "setup.h"
+#include "path-walk.h"
+#include "oid-array.h"
+
+struct path_walk_test_data {
+	uint32_t tree_nr;
+	uint32_t blob_nr;
+};
+
+static int emit_block(const char *path, struct oid_array *oids,
+		      enum object_type type, void *data)
+{
+	struct path_walk_test_data *tdata = data;
+	const char *typestr;
+
+	switch (type) {
+	case OBJ_TREE:
+		typestr = "TREE";
+		tdata->tree_nr += oids->nr;
+		break;
+
+	case OBJ_BLOB:
+		typestr = "BLOB";
+		tdata->blob_nr += oids->nr;
+		break;
+
+	default:
+		BUG("we do not understand this type");
+	}
+
+	for (size_t i = 0; i < oids->nr; i++)
+		printf("%s:%s:%s\n", typestr, path, oid_to_hex(&oids->oid[i]));
+
+	return 0;
+}
+
+int cmd__path_walk(int argc, const char **argv)
+{
+	int argi, res;
+	struct rev_info revs = REV_INFO_INIT;
+	struct path_walk_info info = PATH_WALK_INFO_INIT;
+	struct path_walk_test_data data = { 0 };
+
+	initialize_repository(the_repository);
+	setup_git_directory();
+	revs.repo = the_repository;
+
+	for (argi = 0; argi < argc; argi++) {
+		if (!strcmp(argv[argi], "--"))
+			break;
+	}
+
+	if (argi < argc)
+		setup_revisions(argc - argi, argv + argi, &revs, NULL);
+	else
+		die("usage: test-tool path-walk <options> -- <rev opts>");
+
+	info.revs = &revs;
+	info.path_fn = emit_block;
+	info.path_fn_data = &data;
+
+	res = walk_objects_by_path(&info);
+
+	printf("trees:%d\nblobs:%d\n",
+	       data.tree_nr, data.blob_nr);
+
+	return res;
+}
